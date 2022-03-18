51f3ab94d392736c718f6e77e76e4dbfa1db9178
TuanDao2002 <s3877347@rmit.edu.vn>
2022-03-18 10:58:30 +0700

    update writer

--- src/interfaces/WriterService.java
+++ src/interfaces/WriterService.java
@@ -1,6 +1,12 @@
 package interfaces;
 
+import model.Course;
+import model.Student;
+
+import java.util.ArrayList;
+
 public interface WriterService {
-    void writeCourses();
-    void writeStudents();
+    boolean writeCoursesOfOneStudentOneSem(ArrayList<Course> resultCourseList);
+    boolean writeStudentsOfOneCourseOneSem(ArrayList<Student> resultStudentList);
+    boolean writeCoursesOfOneSem(ArrayList<Course> resultCourseList);
 }
--- src/service/CSVWriterService.java
+++ src/service/CSVWriterService.java
@@ -1,17 +1,76 @@
 package service;
 
 import interfaces.WriterService;
+import model.Course;
+import model.Student;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class CSVWriterService implements WriterService {
-    private String fileName;
+    private final String fileName;
 
-    @Override
-    public void writeCourses() {
+    public CSVWriterService(String id, String semester) {
+        this.fileName = id + "_" + semester;
+    }
 
+    public CSVWriterService(String semester) {
+        this.fileName = semester;
     }
 
     @Override
-    public void writeStudents() {
+    public boolean writeCoursesOfOneStudentOneSem(ArrayList<Course> resultCourseList) {
+        PrintWriter output;
+        try {
+            output = new PrintWriter(new FileWriter("/src/files/" + fileName + "_courses"));
+            output.println("Course ID,Name,Credit");
 
+            for (Course course : resultCourseList) {
+                output.println(course.toString());
+            }
+
+            return true;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    @Override
+    public boolean writeStudentsOfOneCourseOneSem(ArrayList<Student> resultStudentList) {
+        PrintWriter output;
+        try {
+            output = new PrintWriter(new FileWriter("/src/files/" + fileName + "_students"));
+            output.println("Student ID,Name,Birthdate");
+
+            for (Student student : resultStudentList) {
+                output.println(student.toString());
+            }
+
+            return true;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    @Override
+    public boolean writeCoursesOfOneSem(ArrayList<Course> resultCourseList) {
+        PrintWriter output;
+        try {
+            output = new PrintWriter(new FileWriter("/src/files/courses_of_" + fileName));
+            output.println("Course ID,Name,Credit");
+
+            for (Course course : resultCourseList) {
+                output.println(course.toString());
+            }
+
+            return true;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
     }
 }
